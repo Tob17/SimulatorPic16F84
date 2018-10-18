e40c51cc7805534b95f13559d3da6f83bf6b3676
student <student@192.168.101.20>
2018-10-18 13:04:50 +0200

    - finished basic command execution

--- SimulatorPic16F84/src/MainClass.java
+++ SimulatorPic16F84/src/MainClass.java
@@ -28,15 +28,17 @@ public class MainClass {
 	System.out.println("====================================================================");
 	
 	int program[] = parser.convertStringCommandsToInteger(programAsStrings);
-	//for(int i = 0; i < program.length; i++)
-		//System.out.println(program[i]);
 	
+	for(int i = 0; i < program.length -1; i++)
+	{
+	CommandExecution.execute(program[i]);
+	Pic16F84Registers.printWRegister();
+	Pic16F84Registers.printAllFlags();
+	Pic16F84Registers.printPSW();
 	System.out.println("====================================================================");
+	}
 	
 	
-	System.out.println("Value in W Register before command: " + String.format("0x%2X", Pic16F84Registers.W_REGISTER) + "H");
-	CommandExecution.execute(program[0]);
-	System.out.println("Value in W Register after command: " + String.format("0x%2X", Pic16F84Registers.W_REGISTER) + "H");
 	
 	parser.closeFile();
    }
